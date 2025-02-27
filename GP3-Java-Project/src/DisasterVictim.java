d666c5985a62def568b029e71559fcd9607802fb
Sabrina <144979434+realvegetablegummy@users.noreply.github.com>
2025-02-27 00:23:37 -0700

    added DisasterVictim

---
+++ GP3-Java-Project/src/DisasterVictim.java
@@ -0,0 +1,9 @@
+/**
+ * @author      Sabrina Li <a href="mailto:sabrina.li1@ucalgary.ca">sabrina.li1@ucalgary.ca</a>
+ * @version     1.0
+ * @since       1.0
+ */
+
+ package edu.ucalgary.oop;
+
+ 
