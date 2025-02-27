188d6afe41a7680651c04bd5955bcf4f814f4247
cleipoldt <ccpleipoldt@gmail.com>
2025-02-27 01:19:59 -0700

    Added MedicalRecord Class

---
+++ GP3-Java-Project/src/edu/ucalgary/oop/MedicalRecord.java
@@ -0,0 +1,13 @@
+/**
+ * @author      Carl Leipoldt <a href="mailto:carl.leipoldt@ucalgary.ca">carl.leipoldt@ucalgary.ca</a>
+ * @version     1.0
+ * @since       1.0
+ */
+
+
+package edu.ucalgary.oop;
+
+
+public class MedicalRecord {
+
+}
