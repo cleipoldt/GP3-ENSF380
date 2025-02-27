e1184818de873d26138856b3f6faf8195193ff18
cleipoldt <ccpleipoldt@gmail.com>
2025-02-27 01:17:44 -0700

    Finished FamilyRelation class

--- GP3-Java-Project/src/edu/ucalgary/oop/FamilyRelation.java
+++ GP3-Java-Project/src/edu/ucalgary/oop/FamilyRelation.java
@@ -1,6 +1,6 @@
 /**
  * @author      Carl Leipoldt <a href="mailto:carl.leipoldt@ucalgary.ca">carl.leipoldt@ucalgary.ca</a>
- * @version     1.0
+ * @version     1.3
  * @since       1.0
  */
 
@@ -9,5 +9,37 @@
 package edu.ucalgary.oop;
 
 public class FamilyRelation {
+    private DisasterVictim personOne;
+    private String relationshipTo;
+    private DisasterVictim personTwo;
 
+    public FamilyRelation(DisasterVictim personOne, String relationshipTo, DisasterVictim personTwo){
+        this.personOne = personOne;
+        this.relationshipTo = relationshipTo;
+        this.personTwo = personTwo;
+    }
+
+    public DisasterVictim getPersonOne(){
+        return this.personOne;
+    }
+    
+    public void setPersonOne(DisasterVictim personOne){
+        this.personOne = personOne;
+    }
+
+    public String getRelationshipTo(){
+        return this.relationshipTo;
+    }
+
+    public void setRelationshipTo(String relationshipTo){
+        this.relationshipTo = relationshipTo;
+    }
+
+    public DisasterVictim getPersonTwo(){
+        return this.personTwo;
+    }
+
+    public void setPersonTwo(DisasterVictim personTwo){
+        this.personTwo = personTwo;
+    }
 }
